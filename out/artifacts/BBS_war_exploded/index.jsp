62e32efc7ffe46a3d48ccef7273449b54a0e9f81
Yorick <2524964538@qq.com>
2022-04-06 11:11:45 +0800

    首次提交

---
+++ out/artifacts/BBS_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: yurui
+  Date: 2022/3/23
+  Time: 11:10
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
---
+++ src/servlet/LoginServlet.java
@@ -0,0 +1,27 @@
+package servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "LoginServlet", value = "/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        request.setCharacterEncoding("utf-8");
+        String username=request.getParameter("username");
+        String password=request.getParameter("password");
+        out.println("登陆成功<br>");
+        out.println("用户名：" + username + "<br>");
+        out.println("密 码：" + password+"<br>");
+    }
+}
